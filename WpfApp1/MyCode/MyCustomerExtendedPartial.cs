b8ed5e547159175e55166080f58ae87a6490371e
MirjamWeirer <mirjam.weirer@gmx.at>
2022-02-03 11:13:38 +0100

    Projektdateien hinzufügen.

---
+++ WpfApp1/MyCode/MyCustomerExtendedPartial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.MyCode
+{
+    public partial class Customer
+    {
+        public int Age { get; set; }
+
+        private int _MyProperty;
+        public int MyProperty 
+        { 
+            get 
+            { 
+                return _MyProperty;
+            }
+            set
+            {
+                _MyProperty = value;
+            }
+        }
+    }
+}
---
+++ WpfApp1/MyStarterWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MyStarterWindow : Window
+    {
+        public MyStarterWindow()
+        {
+            InitializeComponent();
+            StackPanel myPanel = new StackPanel();
+            Button myButton = new Button();
+            myButton.Content = "Hello World";
+            myPanel.Children.Add(myButton);
+            this.AddChild(myPanel);
+        }
+
+    }   
+}
