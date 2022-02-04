b6b8b1d77ff50b0be8e98b5d5ddd40c2eb446353
MirjamWeirer <mirjam.weirer@gmx.at>
2022-02-04 09:25:03 +0100

    User Interface Übungen

---
+++ DemoCSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCSharp
+{
+    internal class Program
+    {
+        delegate int MethodenAdesseAufIntInt(int a, int b);
+        static void Main(string[] args)
+        {
+            Math m = new Math();
+            int result;
+            result = m.AddNumbers(3, 4);
+            MethodenAdesseAufIntInt adr = m.AddNumbers;
+            result = m.SubNumbers(8, 2);
+
+            adr = m.SubNumbers;
+
+            Rechnen(5, 10, m.AddNumbers);
+            Rechnen(9, 3, m.SubNumbers);
+
+            //Lambda => (u,v) fließt nach / in
+            Rechnen(5, 7, (u, v) => u * v);
+        }
+
+
+        static void Rechnen(int x, int y, MethodenAdesseAufIntInt logik)
+        {
+            var ergebnis = logik(x, y);
+        }
+    }
+
+    class Math
+    {
+        public int AddNumbers(int z1, int z2)
+        {
+            return z1 + z2;
+        }
+
+        public int SubNumbers(int z1, int z2)
+        {
+            return z1 - z2;
+        }
+    }
+}
---
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int merkZahl1()
+        {
+            int z1 = int.Parse(Zahl.Text);
+            return z1;
+        }
+        private int merkZahl2()
+        {
+            int z2 = int.Parse(Zahl.Text);
+            return z2;
+        }
+
+        private string berechnung(string berechnug = null)
+        {
+            return berechnug;
+        }
+
+
+        private void Button_Addition(object sender, RoutedEventArgs e) 
+        {
+            Button addition = (Button)sender;
+            //addition = (Button)addition.Content;
+            //berechnung(addition.ToString());
+            double z1 = Convert.ToDouble(Zahl.Text);
+            double z2 = Convert.ToDouble(Zahl2.Text);
+            double ergebnis = z1 + z2;
+            Ergebnis.Text = ergebnis.ToString();
+        }
+
+        private void Button_Subbtraktion(object sender, RoutedEventArgs e)
+        {
+            Button sub = (Button)sender;
+            double z1 = Convert.ToDouble(Zahl.Text);
+            double z2 = Convert.ToDouble(Zahl2.Text);
+            double ergebnis = z1 - z2;
+            Ergebnis.Text = ergebnis.ToString();
+        }
+
+        private void Button_Division(object sender, RoutedEventArgs e)
+        {
+            Button div = (Button)sender;
+            double z1 = Convert.ToDouble(Zahl.Text);
+            double z2 = Convert.ToDouble(Zahl2.Text);
+            if (z2 == 0)
+            {
+                Zahl.Text = "Error";
+            }
+            double ergebnis = z1 / z2;
+            Ergebnis.Text = ergebnis.ToString();
+        }
+
+        private void Button_Multiplikation(object sender, RoutedEventArgs e)
+        {
+            Button mal = (Button)sender;
+            double z1 = Convert.ToDouble(Zahl.Text);
+            double z2 = Convert.ToDouble(Zahl2.Text);
+            double ergebnis = z1 * z2;
+            Ergebnis.Text = ergebnis.ToString();
+        }
+
+        private void _0(object sender, RoutedEventArgs e)
+        {
+            string z = "0";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+           
+        }
+
+        private void _1(object sender, RoutedEventArgs e)
+        {
+            string z = "1";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _2(object sender, RoutedEventArgs e)
+        {
+            string z = "2";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _3(object sender, RoutedEventArgs e)
+        {
+            string z = "3";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _4(object sender, RoutedEventArgs e)
+        {
+            string z = "4";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _5(object sender, RoutedEventArgs e)
+        {
+            string z = "5";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _6(object sender, RoutedEventArgs e)
+        {
+            string z = "6";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _7(object sender, RoutedEventArgs e)
+        {
+            string z = "7";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _8(object sender, RoutedEventArgs e)
+        {
+            string z = "8";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void _9(object sender, RoutedEventArgs e)
+        {
+            string z = "9";
+            if (Zahl.Text != z && String.IsNullOrEmpty(Zahl.Text))
+            {
+                Zahl.Text = z;
+            }
+            else
+            {
+                Zahl2.Text = z;
+            }
+        }
+
+        private void C(object sender, RoutedEventArgs e)
+        {
+            Zahl.Text = "";
+            Zahl2.Text = "";
+            Ergebnis.Text = "0";
+        }
+    }
+}
