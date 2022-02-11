baddfa35d5f6569a246ce841c1d9a6eb1c195ab8
MirjamWeirer <mirjam.weirer@gmx.at>
2022-02-11 11:02:45 +0100

    EntityFramework und DbContext

---
+++ EntityFrameworkConsoleSample/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkConsoleSample.Models
+{
+    internal class Customer
+    {
+        //Primary Key --- ClassId
+        public int CustomerId { get; set; }
+        public string Firstname { get; set; }
+        public int Lastname { get; set; }
+        public bool Points { get; set; }
+
+    }
+}
---
+++ EntityFrameworkConsoleSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkConsoleSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Nuget-Paket EntityFramework (DBContext, DBSet)
+            //2. Code First: Entity Model - class Customer { CustomerId ...
+            //3. CustomerContext : DBContext --- DBSet
+            //CRUD
+        }
+    }
+}
